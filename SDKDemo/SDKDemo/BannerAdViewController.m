caa17a0413abbba676ae92064b467917bcc1e87a
andot <mabingyao@gmail.com>
2019-09-26 13:43:08 +0800

    更新演示实例代码

--- SDKDemo/SDKDemo/BannerAdViewController.m
+++ SDKDemo/SDKDemo/BannerAdViewController.m
@@ -6,6 +6,8 @@
 //  Copyright © 2019 新义互联（北京）科技有限公司. All rights reserved.
 //
 
+@import AdtalosSDK;
+
 #import "BannerAdViewController.h"
 
 @interface BannerAdViewController ()<AdtalosListener>
--- SDKDemo/SDKDemo/ViewController.m
+++ SDKDemo/SDKDemo/ViewController.m
@@ -6,6 +6,7 @@
 //  Copyright © 2019 新义互联（北京）科技有限公司. All rights reserved.
 //
 
+@import AdtalosSDK;
 #import "ViewController.h"
 #import "BannerAdViewController.h"
 #import "NativeAdViewController.h"
